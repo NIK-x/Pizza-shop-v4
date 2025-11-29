01c916454b89ec5d0c47ddb867cf2469baf695b7
WDEEWEEF <nikitastrakhan@gmail.com>
2025-11-29 15:22:46 +0300

    СОздла начальный дизайн и механки бургер-кнопки, исправил баги

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-import Header from "./Header";
+// import Header from "./Header";
+
+// Получаем элементы
+const burgerButton = document.querySelector('[data-js-burger-button]');
+const overlay = document.querySelector('.header__overlay');
+const menuLinks = document.querySelectorAll('[data-js-menu-link]');
+
+// Функция для переключения меню
+function toggleMenu() {
+    burgerButton.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.html.classList.toggle('is.lock'); 
+}
+
+
+burgerButton.addEventListener('click', toggleMenu);
+
+// Закрытие меню при клике на ссылку
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (overlay.classList.contains('active')) {
+            toggleMenu();
+        }
+    });
+});
